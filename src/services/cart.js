6b55f3aacf7483dcfcb5652b0b15f2df97345e4d
João Pedro Bandeira <jpbandeiradossantos@gmail.com>
2024-10-02 19:44:57 -0300

    Cart estructure

---
+++ src/services/cart.js
@@ -0,0 +1,19 @@
+// Adicionar item ao carrinho
+async function addItemToCart(userCart) {
+
+};
+
+// Deletar item do carrinho
+async function delCartItem(userCart, itemName) {
+    
+};
+
+// Remover item do carrinho
+async function removeCartItem(userCart, index) {
+
+};
+
+// Calcular o total
+async function calculateTotal(userCart) {
+    
+};
